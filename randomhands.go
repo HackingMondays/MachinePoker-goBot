79523bbdba50bc26b5c0365b14dc05f2344e769f
Daniel TISCHER <dos.7182@gmail.com>
2015-01-24 22:42:12 +0100

    fixed bug in example code
    Former-commit-id: d395c97c27586d20971ebd6e594a425e5d919817

--- randomhands.go
+++ randomhands.go
@@ -13,7 +13,6 @@ func main() {
 	fmt.Println(h1)
 	fmt.Println(h2)
 
-
-	// hands := hand.Sort(hand.SortingHigh, h1, h2)
-	// fmt.Println("Winner is:", hands[0].Cards())
+	hands := hand.Sort(hand.SortingHigh, hand.DESC, h1, h2)
+	fmt.Println("Winner is:", hands[0].Cards())
 }
