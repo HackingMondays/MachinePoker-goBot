621491eb45fef06c38adbf156f47cce8f2931f45
Daniel TISCHER <dos.7182@gmail.com>
2015-02-09 21:48:50 +0100

    disable logging
    Former-commit-id: d94e879b4e75d416c020d39faedc85568eb8a8b8

--- logger.go
+++ logger.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	// "io/ioutil"
+	"io/ioutil"
 	"log"
-	"os"
+	// "os"
 )
 
 // Custom logger that can easily be disabled, see:
 // http://www.goinggo.net/2013/11/using-log-package-in-go.html
 
-// var logger = log.New(ioutil.Discard,
-var logger = log.New(os.Stdout,
+var logger = log.New(ioutil.Discard,
+	//var logger = log.New(os.Stdout,
 	"DEBUG: ", 0)
