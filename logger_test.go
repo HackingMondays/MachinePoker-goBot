733b725c8d64625575b13b389788232063a5c293
Daniel TISCHER <dos.7182@gmail.com>
2015-02-25 00:04:29 +0100

    reformat code

--- logger_test.go
+++ logger_test.go
@@ -1,27 +1,26 @@
 package main
 
 import (
-    "testing"
-    "strings"
-    "bytes"
-    "io/ioutil"
+	"bytes"
+	"io/ioutil"
+	"strings"
+	"testing"
 )
 
+func TestLogging(t *testing.T) {
+	// test setup
+	testMessage := "Special Information"
+	out := new(bytes.Buffer)
+	InitLogger(ioutil.Discard, out, ioutil.Discard, ioutil.Discard)
+
+	// log and test
+	Info.Println(testMessage)
+	AssertContains(t, out, "INFO: "+testMessage)
+}
 
 // AssertContains returns true if a buffer contains a line containing message.
 func AssertContains(t *testing.T, out *bytes.Buffer, message string) {
-    if !strings.Contains(out.String(), message) {
-        t.Errorf("Logs do not contain %q, content: %q", message, out.String())
-    }
-}
-
-func TestLogging(t *testing.T) {
-    // test setup
-    testMessage := "Special Information"
-    out := new(bytes.Buffer)
-    InitLogger(ioutil.Discard, out, ioutil.Discard, ioutil.Discard)
-
-    // log and test
-    Info.Println(testMessage)
-    AssertContains(t, out, "INFO: " + testMessage)
+	if !strings.Contains(out.String(), message) {
+		t.Errorf("Logs do not contain %q, content: %q", message, out.String())
+	}
 }
